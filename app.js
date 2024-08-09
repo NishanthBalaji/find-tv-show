c9ac3aaddb933e2f11e56a57e1647157258edd89
Nishanth <97822858+NishanthBalaji@users.noreply.github.com>
2024-08-09 20:03:51 +0530

    Basic functionality is developed

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
+const form = document.querySelector('#form');
 const input = document.querySelector('input');
 const button = document.querySelector('button');
 const display = document.querySelector('#display');
 
-input.addEventListener('input', async function (e) {
-    if (input.value.length > 2) {
-        console.log(input.value);
-        const result = await axios.get(`https://api.tvmaze.com/search/shows?q=${input.value}`);
-        console.log(result.data[0].show.image.medium);
-    }
+form.addEventListener('submit', async function (e) {
+    e.preventDefault();
+    const query = input.value;
+    const result = await axios.get(`https://api.tvmaze.com/search/shows?q=${query}`);
+    appendImg(result.data);
+
 })
+
+const appendImg = (shows) => {
+    console.log(shows);
+    for (let res of shows) {
+        if (res.show.image) {
+            const img = document.createElement('IMG');
+            img.src = res.show.image.medium;
+            display.style.visibility = "unset";
+            display.append(img);
+        }
+
+    }
+}
