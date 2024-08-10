e5d27e003a736f8c35b8f4acf843a795e776fa4d
Nishanth <97822858+NishanthBalaji@users.noreply.github.com>
2024-08-10 13:05:29 +0530

    Improved funtionality

--- app.js
+++ app.js
@@ -2,16 +2,23 @@ const form = document.querySelector('#form');
 const input = document.querySelector('input');
 const button = document.querySelector('button');
 const display = document.querySelector('#display');
+let count = 0;
 
 form.addEventListener('submit', async function (e) {
     e.preventDefault();
+    if (count > 0) {
+        deleteImg();
+    }
+    console.log('outside loop');
     const query = input.value;
     const result = await axios.get(`https://api.tvmaze.com/search/shows?q=${query}`);
     appendImg(result.data);
+    input.value = ' ';
 
 })
 
 const appendImg = (shows) => {
+    count++;
     console.log(shows);
     for (let res of shows) {
         if (res.show.image) {
@@ -20,6 +27,12 @@ const appendImg = (shows) => {
             display.style.visibility = "unset";
             display.append(img);
         }
+    }
+}
 
+function deleteImg() {
+    let imgList = document.querySelectorAll('img');
+    for (let i = 0; i < imgList.length; i++) {
+        imgList[i].remove();
     }
 }
